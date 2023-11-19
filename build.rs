3a8ec703cf7bcdfa4b615200ebc6b0d1bd31094a
David Schramm <david.schramm@live.com>
2023-11-19 12:39:28 +0100

    chdir to OUT_DIR

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
-fn main() {
+use std::env;
+use std::io::Error as IoError;
+
+fn main() -> Result<(), IoError> {
+    if let Ok(path) = env::var("OUT_DIR") {
+        env::set_current_dir(path)?;
+    }
+
     embuild::espidf::sysenv::output();
+
+    Ok(())
 }
