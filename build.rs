52f67339067b67ef1415eb0aa031895856714102
David Schramm <david.schramm@live.com>
2023-11-19 13:12:02 +0100

    use var

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::io::Error as IoError;
 
 fn main() -> Result<(), IoError> {
-    if let Ok(path) = env::var_os("OUT_DIR") {
+    if let Ok(path) = env::var("OUT_DIR") {
         env::set_current_dir(path)?;
     }
 
